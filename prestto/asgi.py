6f67a84be5511c6ba65c8d7e6a598f74c17ac512
maro <marookegbero@gmail.com>
2021-10-12 04:43:29 +0100

    initial commit

---
+++ prestto/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for prestto project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prestto.settings')
+
+application = get_asgi_application()
