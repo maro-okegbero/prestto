90dbac6ced50a77e03be99473a5ed6022a2b006e
maro <marookegbero@gmail.com>
2021-10-12 13:29:54 +0100

    wrote view for homepage

--- prestto/website/views.py
+++ prestto/website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def homepage(request):
+    """
+    renders the homepage
+    :param request:
+    :return:
+    """
+    return render(request, template_name="website/index.html", context=dict())
