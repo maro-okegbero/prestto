9a32d733cf733eae37f3a9c122363ef2506f1477
maro <marookegbero@gmail.com>
2021-10-21 15:31:23 +0100

    writing register serializers.py

--- prestto/api/serializers.py
+++ prestto/api/serializers.py
@@ -66,35 +66,24 @@ class UserSerializer(serializers.ModelSerializer):
     points = serializers.IntegerField(read_only=True)
     referral_code = serializers.CharField(read_only=True)
 
-    def create(self, validated_data):
-        """
-        serializer.save() calls this function
-        """
-        token = token_generator()
-        referral_code = generate_referral_code()
-        validated_data['email_token'] = token
-        validated_data['referral_code'] = referral_code
-        ref_code = validated_data.get("referee", None)
-        if ref_code:
-            try:
-                referee = AffiliateInfo.objects.get(user__referral_code=ref_code)
-                referee.total_signups += 1
-                referee.save()
-                validated_data['referee'] = ref_code
-            except [AffiliateInfo.DoesNotExist, Exception] as e:
-                print(e, "Error for affiliate processing....................")
-                pass
-        user = user_model().objects.create_user(**validated_data)
-
-        # create an affiliate profile for the user
-        affiliate_info = AffiliateInfo.objects.create(user=user)
-
-        name = user.fullname
-        email = validated_data.get("email")
-        print("I'm about to send a mail initiation...............................................")
-        send_email_verification_pin(email=email, pin=token, name=name)
-
-        return user
+    #
+    # def create(self, validated_data):
+    #     """
+    #     serializer.save() calls this function
+    #     """
+    #     token = token_generator()
+    #     referral_code = generate_referral_code()
+    #     validated_data['email_token'] = token
+    #     validated_data['referral_code'] = referral_code
+    #     ref_code = validated_data.get("referee", None)
+    #
+    #     user = user_model().objects.create_user(**validated_data)
+    #     name = user.fullname
+    #     email = validated_data.get("email")
+    #     print("I'm about to send a mail initiation...............................................")
+    #     send_email_verification_pin(email=email, pin=token, name=name)
+    #
+    #     return user
 
     # def validate_email_address(self, email, token):
     class Meta:
