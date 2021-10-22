9575dd601a19ec021349ed9215a0c421be5ec3e0
maro <marookegbero@gmail.com>
2021-10-22 02:04:18 +0100

    login view

--- prestto/api/views.py
+++ prestto/api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from prestto.api.serializers import UserSerializer
+from prestto.api.serializers import UserSerializer, LoginSerializer
 
 
 @csrf_exempt
@@ -24,5 +25,20 @@ def register_user(request):
             json['token'] = token
             return Response(json, status=status.HTTP_201_CREATED)
 
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_user(request):
+    """
+    Log a user in using the username and password as the authentication credentials
+    """
+    data = request.data
+    print(data, "User===========")
+    serializer = LoginSerializer(data=data)
+    if serializer.is_valid():
+        return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
