135a66124148d87cfa7d432ff09cabe08881c3bc
maro <marookegbero@gmail.com>
2021-10-22 02:02:02 +0100

    fixes

--- prestto/api/serializers.py
+++ prestto/api/serializers.py
@@ -6,7 +6,6 @@ from django.contrib.auth import authenticate
 from django.utils.translation import gettext_lazy, ugettext_lazy
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from .models import *
 from django.db.models import ObjectDoesNotExist
 
 
